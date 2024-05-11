9040858cb4faf2cc6c9dbcdbc9bd343e610298ed
Mehyad <43511859+myadegari@users.noreply.github.com>
2024-05-11 13:31:22 +0330

    🙄

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
 export const baseURL = 'review-assist/';
-export const isolateBaseURL ='/review-assist/'
+// export const isolateBaseURL ='/review-assist/'
+export const isolateBaseURL ='/'
 // export const baseURL = '/';
